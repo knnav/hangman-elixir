d98085bb503904c53eb5a05cca52b3f993c8aaed
knorenbergs <knorenbergs@pagerduty.com>
2024-08-02 18:37:13 -0400

    Refactor word_list

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,12 +1,11 @@
 defmodule Dictionary do
-  def word_list do
-    "assets/words"
+
+  @word_list "assets/words"
     |> File.read!()
     |> String.split()
-  end
 
   def random_word do
-    word_list()
+    @word_list
     |> Enum.random()
   end
 end
